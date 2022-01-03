d4bf4876221516b7605f923e9985f444eefbc52f
Mohd Faizan <48771856+zanhd@users.noreply.github.com>
2022-01-03 11:54:02 +0530

    Update Number Game.cpp

--- Number Game.cpp
+++ Number Game.cpp
@@ -2,66 +2,28 @@
 #define ll long long int
 using namespace std;
 
-vector<vector<pair<ll, ll>>> go(vector<ll> a)
-{
-	vector<vector<pair<ll, ll>>> group;
-
-	if (a.size() == 0) return group;
-
-	for (ll i = 0; i < a.size(); i++)
-	{
-		for (ll j = 0; j < a.size(); j++)
-		{
-			if (i == j) continue;
-
-			//choose this pair
-			vector<ll> b;
-			for (ll k = 0; k < a.size(); k++)
-			{
-				if (k == i || k == j) continue;
-				b.push_back(a[k]);
+int dp[8][16384] = {};
+int maxScore(vector<int>& a, int i = 1, int mask = 0) {
+	if (i > a.size() / 2)
+		return 0;
+	if (!dp[i][mask])
+		for (int j = 0; j < a.size(); ++j)
+			for (auto k = j + 1; k < a.size(); ++k) {
+				int new_mask = (1 << j) + (1 << k);
+				if ((mask & new_mask) == 0)
+					dp[i][mask] = max(dp[i][mask], i * __gcd(a[j], a[k]) + maxScore(a, i + 1, mask + new_mask));
 			}
-
-			vector<vector<pair<ll, ll>>> now = go(b);
-
-			if (!now.size())
-			{
-				vector<pair<ll, ll>> x; x.push_back({a[i], a[j]});
-				group.push_back(x);
-			}
-			else
-			{
-				for (auto x : now)
-				{
-					x.insert(x.begin(), {a[i], a[j]});
-					group.push_back(x);
-				}
-			}
-		}
-	}
-
-	return group;
-
+	return dp[i][mask];
 }
 
 int main()
 {
-	ll n; cin >> n;
-	vector<ll> a(2 * n);
-	for (ll i = 0; i < 2 * n; i++)
+	int n; cin >> n;
+	vector<int> a(2 * n);
+	for (int i = 0; i < 2 * n; i++)
 		cin >> a[i];
 
-	vector < vector<pair<ll, ll>>> group = go(a);
-
-	ll ans = 0;
-	for (auto b : group)
-	{
-		ll now = 0;
-		for (ll i = 0; i < b.size(); i++)
-			now += __gcd(b[i].first, b[i].second) * (i + 1);
-		ans = max(ans, now);
-	}
-	cout << ans;
+	cout << maxScore(a) << endl;
 
 	return 0;
 }
